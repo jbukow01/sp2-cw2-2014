1fc23da27db570c8f1e6a86eca7bddc9499a325c
Jarek <jbukow01@dcs.bbk.ac.uk>
2014-11-08 20:15:55 +0000

    indentation adjustment

--- src/coursework2/Fraction.java
+++ src/coursework2/Fraction.java
@@ -4,80 +4,85 @@ package coursework2;
  * Created by keith for the second coursework assignment.
  */
 public class Fraction {
-    private int numerator;
-    private int denominator;
+	private int numerator;
+	private int denominator;
 
-    public Fraction(int num, int denom) {
-        if (denom == 0) {
-            System.out.println("Invalid fraction with denominator 0"); 
-	    // this should use exceptions
-            return;
-        }
-        int gcd = myGcd(num, denom);
-        setNumerator(num / gcd);
-        setDenominator(denom / gcd);
-    }
+	public Fraction(int num, int denom) {
+		if (denom == 0) {
+			System.out.println("Invalid fraction with denominator 0");
+			// this should use exceptions
+			return;
+		}
+		int gcd = myGcd(num, denom);
+		setNumerator(num / gcd);
+		setDenominator(denom / gcd);
+	}
 
-    @Override
-    public String toString() {
-        return "" + getNumerator() + '/' + getDenominator();
-    }
+	@Override
+	public String toString() {
+		return "" + getNumerator() + '/' + getDenominator();
+	}
 
-    public int getNumerator() {
-        return numerator;
-    }
+	public int getNumerator() {
+		return numerator;
+	}
 
-    public void setNumerator(int num) {
-        numerator = num;
-    }
+	public void setNumerator(int num) {
+		numerator = num;
+	}
 
-    public int getDenominator() {
-        return denominator;
-    }
+	public int getDenominator() {
+		return denominator;
+	}
 
-    public void setDenominator(int num) {
-        denominator = num;
-    }
+	public void setDenominator(int num) {
+		denominator = num;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        Fraction fraction = (Fraction) o;
+		Fraction fraction = (Fraction) o;
 
-        if (getDenominator() != fraction.getDenominator()) return false;
-        if (getNumerator() != fraction.getNumerator()) return false;
+		if (getDenominator() != fraction.getDenominator())
+			return false;
+		if (getNumerator() != fraction.getNumerator())
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = numerator;
-        result = 31 * result + denominator;
-        return result;
-    }
-    
-    public Fraction add(Fraction other) {
-    	int num = (this.getNumerator() * other.getDenominator()) + (this.getDenominator() * other.getNumerator());
-    	int denom = this.getDenominator() * other.getDenominator();
-    	return new Fraction(num, denom);
-    }
+	@Override
+	public int hashCode() {
+		int result = numerator;
+		result = 31 * result + denominator;
+		return result;
+	}
 
-    public Fraction multiply(Fraction other) {
+	public Fraction add(Fraction other) {
+		int num = (this.getNumerator() * other.getDenominator())
+				+ (this.getDenominator() * other.getNumerator());
+		int denom = this.getDenominator() * other.getDenominator();
+		return new Fraction(num, denom);
+	}
 
-        int num = this.getNumerator() * other.getNumerator();
-        int denom = this.getDenominator() * other.getDenominator();
-        return new Fraction(num, denom);
-    }
+	public Fraction multiply(Fraction other) {
 
-    private int myGcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        return a;
-    }
+		int num = this.getNumerator() * other.getNumerator();
+		int denom = this.getDenominator() * other.getDenominator();
+		return new Fraction(num, denom);
+	}
+
+	private int myGcd(int a, int b) {
+		while (b != 0) {
+			int t = b;
+			b = a % b;
+			a = t;
+		}
+		return a;
+	}
 }
--- src/coursework2/FractionTest.java
+++ src/coursework2/FractionTest.java
@@ -2,35 +2,38 @@ package coursework2;
 
 /**
  * Created by keith for the second coursework assignment.
- *
+ * 
  * All the tests should be (re-)written using JUnit
  */
 public class FractionTest {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // test divide by zero - should print an error and exit
-        new Fraction(1, 2);
-        // test multiply
-	Fraction f = new Fraction(3,10);
-	Fraction g = new Fraction(1,2);
-	Fraction h = new Fraction(3,5);
-	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
-	Fraction a = new Fraction(1,2);
-	Fraction b = new Fraction(4,16);
-	Fraction c = new Fraction(3,12);
-	if (!a.equals(b.add(c))) System.out.println("Addition failed");
-        // test equals
-	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
-	test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
-	test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
-	test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
-	//test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
+		// test divide by zero - should print an error and exit
+		new Fraction(1, 2);
+		// test multiply
+		Fraction f = new Fraction(3, 10);
+		Fraction g = new Fraction(1, 2);
+		Fraction h = new Fraction(3, 5);
+		if (!f.equals(g.multiply(h)))
+			System.out.println("Multiply failed");
+		// test addition
+		Fraction a = new Fraction(1, 2);
+		Fraction b = new Fraction(4, 16);
+		Fraction c = new Fraction(3, 12);
+		if (!a.equals(b.add(c)))
+			System.out.println("Addition failed");
+		// test equals
+		test(new Fraction(1, 2), new Fraction(1, 2), "error test 1");
+		test(new Fraction(1, 2), new Fraction(3, 6), "error test 2");
+		test(new Fraction(-1, 2), new Fraction(1, -2), "error test 3");
+		test(new Fraction(-1, -2), new Fraction(1, 2), "error test 4");
+		// test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
 
-        // extend with extra tests
-    }
+		// extend with extra tests
+	}
 
-    static void test(Fraction f1, Fraction f2, String msg){
-    	if (! f1.equals(f2))
-		System.out.println(msg);
-    }
+	static void test(Fraction f1, Fraction f2, String msg) {
+		if (!f1.equals(f2))
+			System.out.println(msg);
+	}
 }
