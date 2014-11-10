a6abca125fdbb306b483c6f24d4c9b9f9f1c71d1
Jarek <jbukow01@dcs.bbk.ac.uk>
2014-11-10 23:29:42 +0000

    added fraction calculator test class to take user input

---
+++ src/coursework2/FractionCalculatorTest.java
@@ -0,0 +1,14 @@
+package coursework2;
+
+import java.util.Scanner;
+
+public class FractionCalculatorTest {
+	
+	public static void main(String[] args) {
+		Fraction temp = new Fraction(0, 1);
+		System.out.println("Input: ");
+		Scanner input = new Scanner(System.in);
+		String calc = input.nextLine();
+		System.out.println(FractionCalculator.evaluate(temp, calc));
+	}
+}
